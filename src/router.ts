e199005be9f0fed6c1af0e2b0167088831f31534
Francis Pion <francispion@hotmail.com>
2024-04-23 19:04:11 -0400

    Code Review.

--- src/router.ts
+++ src/router.ts
@@ -5,6 +5,7 @@ import HomeView from "./views/HomeView.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // Home
     {
       name: "Home",
       path: "/",
