aaedae9bb321585f0c27b94269f8855bd8cdeff7
Francis Pion <francispion@hotmail.com>
2024-04-23 18:38:58 -0400

    Implemented Logitar Vue3 Template.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import VueDevTools from "vite-plugin-vue-devtools";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import { defineConfig } from "vite";
@@ -7,7 +8,7 @@ import packageJson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), VueDevTools()],
   define: {
     "import.meta.env.VITE_APP_VERSION": JSON.stringify(packageJson.version),
   },
