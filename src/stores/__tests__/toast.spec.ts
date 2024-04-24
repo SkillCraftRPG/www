04a50cbcd6aded89a8958cb7b2b613334fb4b8a6
Francis Pion <francispion@hotmail.com>
2024-04-24 00:59:13 -0400

    Fixed tests.

--- src/stores/__tests__/toast.spec.ts
+++ src/stores/__tests__/toast.spec.ts
@@ -37,13 +37,13 @@ describe("toastStore", () => {
     });
     const toast: ToastOptions | undefined = toasts.toasts.at(0);
     if (toast) {
-      expect(toast.close).toBe("Close");
+      expect(toast.close).toBe("Fermer");
       expect(toast.duration).toBe(15000);
       expect(toast.fade).toBe(true);
       expect(toast.id).toMatch(/^toast_/);
       expect(toast.solid).toBe(true);
-      expect(toast.text).toBe("The requested page could not be found.");
-      expect(toast.title).toBe("Success!");
+      expect(toast.text).toBe("La page demandée n’a pu être trouvée.");
+      expect(toast.title).toBe("Succès !");
       expect(toast.variant).toBe("info");
     } else {
       expect(toast).toBeDefined();
@@ -57,8 +57,8 @@ describe("toastStore", () => {
     toasts.success("notFound.lead");
     const toast: ToastOptions | undefined = toasts.toasts.at(0);
     if (toast) {
-      expect(toast.text).toBe("Page Not Found");
-      expect(toast.title).toBe("Success!");
+      expect(toast.text).toBe("Page introuvable");
+      expect(toast.title).toBe("Succès !");
       expect(toast.variant).toBe("success");
     } else {
       expect(toast).toBeDefined();
@@ -72,8 +72,8 @@ describe("toastStore", () => {
     toasts.warning("toasts.warning.signedOut");
     const toast: ToastOptions | undefined = toasts.toasts.at(0);
     if (toast) {
-      expect(toast.text).toBe("You have been signed-out, please try sign-in to your account again.");
-      expect(toast.title).toBe("Warning!");
+      expect(toast.text).toBe("Vous avez été déconnecté, veuillez vous reconnecter à votre compte.");
+      expect(toast.title).toBe("Attention !");
       expect(toast.variant).toBe("warning");
     } else {
       expect(toast).toBeDefined();
@@ -87,8 +87,8 @@ describe("toastStore", () => {
     toasts.error("notFound.link");
     const toast: ToastOptions | undefined = toasts.toasts.at(0);
     if (toast) {
-      expect(toast.text).toBe("Go to Home");
-      expect(toast.title).toBe("Error!");
+      expect(toast.text).toBe("Aller à l’accueil");
+      expect(toast.title).toBe("Erreur !");
       expect(toast.variant).toBe("danger");
     } else {
       expect(toast).toBeDefined();
@@ -102,8 +102,8 @@ describe("toastStore", () => {
     toasts.error();
     const toast: ToastOptions | undefined = toasts.toasts.at(0);
     if (toast) {
-      expect(toast.text).toBe("An unexpected error occurred. Please try again later or contact our support team.");
-      expect(toast.title).toBe("Error!");
+      expect(toast.text).toBe("Une erreur inattendue s’est produite. Veuillez réessayer plus tard ou contacter notre équipe de support.");
+      expect(toast.title).toBe("Erreur !");
       expect(toast.variant).toBe("danger");
     } else {
       expect(toast).toBeDefined();
