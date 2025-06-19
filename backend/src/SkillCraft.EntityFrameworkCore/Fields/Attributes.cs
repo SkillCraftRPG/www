6cfa4d9ca85085dad73748d531372ada68370e52
Francis Pion <francispion@hotmail.com>
2025-06-19 11:34:12 -0400

    fix

--- backend/src/SkillCraft.EntityFrameworkCore/Fields/Attributes.cs
+++ backend/src/SkillCraft.EntityFrameworkCore/Fields/Attributes.cs
@@ -2,7 +2,7 @@
 
 internal static class Attributes
 {
-  public static readonly Guid Description = Guid.Parse("be556b54e025d010-d998-4566-80b7-530e1e9410b0");
+  public static readonly Guid Description = Guid.Parse("e025d010-d998-4566-80b7-530e1e9410b0");
   public static readonly Guid Slug = Guid.Parse("b265b172-48e4-47be-8284-fe01834bf233");
   public static readonly Guid Summary = Guid.Parse("50b04797-2e52-477a-90b9-d00cb7a33a88");
 }
