a8abfca020341df2281a83faeb9cf39b508ecd03
Francis Pion <francispion@hotmail.com>
2025-06-25 09:57:06 -0400

    Talent Description Text. (#13)

--- frontend/pages/regles/talents/index.vue
+++ frontend/pages/regles/talents/index.vue
@@ -3,11 +3,70 @@
     <main class="container">
       <h1>{{ title }}</h1>
       <AppBreadcrumb :active="title" />
-      <!-- TODO(fpion): explanation text -->
-      <p>{{ "[…]" }}</p>
+      <p>
+        Une des facettes de la <NuxtLink to="/regles/personnages/progression">progression</NuxtLink> de personnage se matérialise par des talents, des capacités
+        acquises au fil de ses aventures.
+      </p>
+      <p>
+        Deux personnages suivant un parcours similaire peuvent totalement différer dans leurs choix de talents, ce qui permet au joueur de personnaliser son
+        personnage en fonction de ses préférences.
+      </p>
+      <h2 class="h3">Points de talent</h2>
+      <p>
+        Dès sa <NuxtLink to="/regles/personnages/creation">création</NuxtLink>, un personnage reçoit 12 points de talent. Il obtient également 4 points à chaque
+        fois qu’il progresse à un <NuxtLink to="/regles/personnages/progression/niveau">niveau</NuxtLink> supérieur. Il peut dépenser ces points afin d’acquérir
+        de nouveaux talents. Le coût en points d’un talent dépend de son tiers.
+      </p>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Tiers du talent</th>
+            <th scope="col">Coût en points</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>0</td>
+            <td>2</td>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>5</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3 class="h5">Rabais</h3>
+      <p>
+        Lorsqu’un personnage bénéficie d’un rabais sur un talent, alors il pourra acquérir ce talent en dépensant 1 point de talent de moins que le nombre qui
+        lui serait normalement nécessaire. Il peut bénéficier de plusieurs rabais sur un même talent, ce qui peut lui permettre d’acquérir un talent à très
+        faible coût ou gratuitement.
+      </p>
+      <h2 class="h3">Conditions d’acquisition</h2>
+      <p>Afin d’acquérir un talent, un personnage doit satisfaire les conditions suivantes.</p>
+      <ul>
+        <li>Son <NuxtLink to="/regles/personnages/progression/tiers">tiers</NuxtLink> de personnage doit être supérieur ou égal au tiers du talent.</li>
+        <li>Si le talent requiert un autre talent, alors le personnage doit préalablement avoir acquis le talent requis.</li>
+        <li>Il doit dépenser les points de talent nécessaires.</li>
+        <li>Le personnage doit satisfaire toute condition additionnelle spécifiée par le talent.</li>
+      </ul>
+      <h2 class="h3">Achats multiples</h2>
+      <p>
+        Sauf indication contraire, un talent ne peut être acquis qu’une seule fois. Seuls les talents portant la mention
+        <strong>Achats multiples</strong> peuvent être acquis plusieurs fois. Chaque acquisition doit satisfaire les conditions indiquées ci-dessus.
+      </p>
+      <p>S’il possède un ou plusieurs rabais sur ce talent, alors chaque acquisition bénéficie du rabais.</p>
     </main>
-    <section class="container-fluid">
-      <table v-if="talents.length" class="table table-striped">
+    <section v-if="talents.length" class="container-fluid">
+      <h2 class="h3">Liste des talents</h2>
+      <table class="table table-striped">
         <thead>
           <tr>
             <th scope="col">Tiers</th>
