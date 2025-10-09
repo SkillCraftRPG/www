d53a730a07bc2c3cabafc0b344ea6937c1dc3615
Francis Pion <francispion@hotmail.com>
2025-10-09 01:10:28 -0400

    init

--- backend/src/SkillCraft.Cms/Program.cs
+++ backend/src/SkillCraft.Cms/Program.cs
@@ -1,8 +1,11 @@
-﻿namespace SkillCraft.Cms;
+﻿using Krakenar.Core.Caching;
+using Krakenar.Core.Configurations;
+
+namespace SkillCraft.Cms;
 
 internal class Program
 {
-  public static void Main(string[] args)
+  public static async Task Main(string[] args)
   {
     WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
     IConfiguration configuration = builder.Configuration;
@@ -13,7 +16,20 @@ internal class Program
     WebApplication application = builder.Build();
 
     startup.Configure(application);
+    await LoadConfigurationAsync(application);
 
     application.Run();
+  }
+  private static async Task LoadConfigurationAsync(WebApplication application, CancellationToken cancellationToken = default)
+  {
+    using IServiceScope scope = application.Services.CreateScope();
+
+    ICacheService cacheService = scope.ServiceProvider.GetRequiredService<ICacheService>();
+    IConfigurationQuerier configurationQuerier = scope.ServiceProvider.GetRequiredService<IConfigurationQuerier>();
+    IConfigurationRepository configurationRepository = scope.ServiceProvider.GetRequiredService<IConfigurationRepository>();
+
+    Configuration configuration = await configurationRepository.LoadAsync(cancellationToken)
+      ?? throw new InvalidOperationException("The configuration has not been initialized");
+    cacheService.Configuration = await configurationQuerier.ReadAsync(configuration, cancellationToken);
   }
 }
