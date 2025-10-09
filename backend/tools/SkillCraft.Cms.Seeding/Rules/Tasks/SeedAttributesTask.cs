21b9beebc6471ebaacef4940c30a553b5d2516dd
Francis Pion <francispion@hotmail.com>
2025-10-08 21:23:01 -0400

    defaults

---
+++ backend/tools/SkillCraft.Cms.Seeding/Rules/Tasks/SeedAttributesTask.cs
@@ -0,0 +1,36 @@
+﻿using Krakenar.Contracts.Contents;
+using Krakenar.Core;
+
+namespace SkillCraft.Cms.Seeding.Rules.Tasks;
+
+internal class SeedAttributesTask : SeedingTask
+{
+  public override string? Description => $"Seeds the Attributes contents.";
+}
+
+internal class SeedAttributesTaskHandler : ICommandHandler<SeedAttributesTask, TaskResult>
+{
+  private readonly IContentService _contentService;
+  private readonly ILogger<SeedAttributesTaskHandler> _logger;
+
+  public SeedAttributesTaskHandler(IContentService contentService, ILogger<SeedAttributesTaskHandler> logger)
+  {
+    _contentService = contentService;
+    _logger = logger;
+  }
+
+  public async Task<TaskResult> HandleAsync(SeedAttributesTask command, CancellationToken cancellationToken)
+  {
+    string json = await File.ReadAllTextAsync("Rules/data/attributes.json", Encoding.UTF8, cancellationToken);
+    IEnumerable<AttributeDto>? attributes = SeedingSerializer.Deserialize<IEnumerable<AttributeDto>>(json);
+    if (attributes is not null)
+    {
+      foreach (AttributeDto attribute in attributes)
+      {
+
+      }
+    }
+
+    return new TaskResult();
+  }
+}
