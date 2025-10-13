3cf5e03db30e44aecf25332ce2204731fb846a10
Francis Pion <francispion@hotmail.com>
2025-10-13 15:28:48 -0400

    api_progress

---
+++ backend/src/SkillCraft.Cms.Core/Progress/Queries/ReadProgressQuery.cs
@@ -0,0 +1,64 @@
+﻿using Krakenar.Contracts.Contents;
+using Krakenar.Contracts.Fields;
+using Krakenar.Contracts.Search;
+using Krakenar.Core;
+using Krakenar.Core.Contents;
+using SkillCraft.Cms.Core.Progress.Models;
+
+namespace SkillCraft.Cms.Core.Progress.Queries;
+
+internal record ReadProgress : IQuery<ProgressModel>;
+
+internal class ReadProgressHandler : IQueryHandler<ReadProgress, ProgressModel>
+{
+  private static readonly Dictionary<Guid, Action<ProgressModel, FieldValue>> _handlers = new()
+  {
+    [ProgressType.Characters] = (progress, field) => progress.Characters = Parse(field.Value),
+    [ProgressType.Attributes] = (progress, field) => progress.Attributes = Parse(field.Value),
+    [ProgressType.Statistics] = (progress, field) => progress.Statistics = Parse(field.Value),
+    [ProgressType.Skills] = (progress, field) => progress.Skills = Parse(field.Value),
+    [ProgressType.Species] = (progress, field) => progress.Species = Parse(field.Value),
+    [ProgressType.Customizations] = (progress, field) => progress.Customizations = Parse(field.Value),
+    [ProgressType.Castes] = (progress, field) => progress.Castes = Parse(field.Value),
+    [ProgressType.Educations] = (progress, field) => progress.Educations = Parse(field.Value),
+    [ProgressType.Talents] = (progress, field) => progress.Talents = Parse(field.Value),
+    [ProgressType.Specializations] = (progress, field) => progress.Specializations = Parse(field.Value),
+    [ProgressType.Languages] = (progress, field) => progress.Languages = Parse(field.Value),
+    [ProgressType.Equipment] = (progress, field) => progress.Equipment = Parse(field.Value),
+    [ProgressType.Adventure] = (progress, field) => progress.Adventure = Parse(field.Value),
+    [ProgressType.Combat] = (progress, field) => progress.Combat = Parse(field.Value),
+    [ProgressType.Magic] = (progress, field) => progress.Magic = Parse(field.Value),
+    [ProgressType.Annexes] = (progress, field) => progress.Annexes = Parse(field.Value)
+  };
+
+  private readonly IPublishedContentQuerier _publishedContentQuerier;
+
+  public ReadProgressHandler(IPublishedContentQuerier publishedContentQuerier)
+  {
+    _publishedContentQuerier = publishedContentQuerier;
+  }
+
+  public async Task<ProgressModel> HandleAsync(ReadProgress query, CancellationToken cancellationToken)
+  {
+    SearchPublishedContentsPayload payload = new();
+    payload.ContentType.Uids.Add(ProgressType.ContentTypeId);
+    SearchResults<PublishedContentLocale> locales = await _publishedContentQuerier.SearchAsync(payload, cancellationToken);
+    PublishedContentLocale locale = locales.Items.Single();
+
+    ProgressModel progress = new();
+    foreach (FieldValue fieldValue in locale.FieldValues)
+    {
+      if (_handlers.TryGetValue(fieldValue.Id, out Action<ProgressModel, FieldValue>? handler))
+      {
+        handler(progress, fieldValue);
+      }
+    }
+    return progress;
+  }
+
+  private static double Parse(string value)
+  {
+    value = value.Split('=').Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => x.Trim()).Last();
+    return double.TryParse(value, NumberStyles.Any, CultureInfo.InvariantCulture, out double progress) ? progress : 0;
+  }
+}
---
+++ backend/src/SkillCraft.Cms/Controllers/ProgressController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkillCraft.Cms.Core.Progress;
+using SkillCraft.Cms.Core.Progress.Models;
+
+namespace SkillCraft.Cms.Controllers;
+
+[ApiController]
+[Route("api/progress")]
+public class ProgressController : ControllerBase
+{
+  private readonly IProgressService _progressService;
+
+  public ProgressController(IProgressService progressService)
+  {
+    _progressService = progressService;
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<ProgressModel>> GetAsync(CancellationToken cancellationToken)
+  {
+    ProgressModel progress = await _progressService.ReadAsync(cancellationToken);
+    return Ok(progress);
+  }
+}
