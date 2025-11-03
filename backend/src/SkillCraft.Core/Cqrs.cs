4ba11710d0d340a927305faeb32e821dabe9b1af
Francis Pion <francispion@hotmail.com>
2025-11-03 01:33:10 -0500

    fix

--- backend/src/SkillCraft.Core/Cqrs.cs
+++ backend/src/SkillCraft.Core/Cqrs.cs
@@ -4,14 +4,14 @@ namespace SkillCraft.Core;
 
 public interface ICommand<T>;
 
-public interface ICommandHandler<TCommand, TResult> where TCommand : ICommand<TResult>
-{
-  Task<TResult> HandleAsync(TCommand command, CancellationToken cancellationToken = default);
-}
-
 public interface ICommandBus
 {
   Task<TResult> ExecuteAsync<TResult>(ICommand<TResult> command, CancellationToken cancellationToken = default);
+}
+
+public interface ICommandHandler<TCommand, TResult> where TCommand : ICommand<TResult>
+{
+  Task<TResult> HandleAsync(TCommand command, CancellationToken cancellationToken = default);
 }
 
 public interface IEventHandler<T> where T : IEvent
