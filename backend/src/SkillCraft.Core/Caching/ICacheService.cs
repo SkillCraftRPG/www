8fbd11fa90e090829ce0ce8006942fb075842b89
Francis Pion <francispion@hotmail.com>
2025-11-03 01:38:07 -0500

    fixes

---
+++ backend/src/SkillCraft.Core/Caching/ICacheService.cs
@@ -0,0 +1,11 @@
+﻿using Krakenar.Contracts.Actors;
+using Logitar.EventSourcing;
+
+namespace SkillCraft.Core.Caching;
+
+public interface ICacheService
+{
+  Actor? GetActor(ActorId actorId);
+  void RemoveActor(ActorId actorId);
+  void SetActor(Actor actor);
+}
--- backend/src/SkillCraft.Infrastructure/Actors/ActorService.cs
+++ backend/src/SkillCraft.Infrastructure/Actors/ActorService.cs
@@ -1,5 +1,6 @@
 ﻿using Krakenar.Contracts.Actors;
 using Logitar.EventSourcing;
+using SkillCraft.Core.Caching;
 
 namespace SkillCraft.Infrastructure.Actors;
 
@@ -10,10 +11,42 @@ public interface IActorService
 
 internal class ActorService : IActorService
 {
+  private readonly ICacheService _cacheService;
+
+  public ActorService(ICacheService cacheService)
+  {
+    _cacheService = cacheService;
+  }
+
   public Task<IReadOnlyDictionary<ActorId, Actor>> FindAsync(IEnumerable<ActorId> ids, CancellationToken cancellationToken)
   {
-    Dictionary<ActorId, Actor> actors = [];
-    // TODO(fpion): implement
+    int capacity = ids.Count();
+    Dictionary<ActorId, Actor> actors = new(capacity);
+    HashSet<ActorId> missingIds = new(capacity);
+
+    foreach (ActorId id in ids)
+    {
+      Actor? actor = _cacheService.GetActor(id);
+      if (actor is null)
+      {
+        missingIds.Add(id);
+      }
+      else
+      {
+        actors[id] = actor;
+      }
+    }
+
+    if (missingIds.Count > 0)
+    {
+      // TODO(fpion): implement
+    }
+
+    foreach (Actor actor in actors.Values)
+    {
+      _cacheService.SetActor(actor);
+    }
+
     return Task.FromResult((IReadOnlyDictionary<ActorId, Actor>)actors.AsReadOnly());
   }
 }
