120d5e2564075823d53f8339cc8487b9c21037fd
Francis Pion <francispion@hotmail.com>
2025-11-15 22:15:09 -0500

    Implemented Spell Pages. (#111)

---
+++ frontend/components/spell/Card.vue
@@ -0,0 +1,11 @@
+<template>
+  <LinkCard :text="spell.summary" :title="spell.name" :to="`/regles/magie/pouvoirs/${spell.slug}`" />
+</template>
+
+<script lang="ts" setup>
+import type { Spell } from "~/types/magic";
+
+defineProps<{
+  spell: Spell;
+}>();
+</script>
---
+++ frontend/components/spell/Info.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h3 mb-3">
+    <TarBadge variant="secondary">Tiers&nbsp;:&nbsp;{{ $n(spell.tier, "integer") }}</TarBadge>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Spell } from "~/types/magic";
+
+defineProps<{
+  spell: Spell;
+}>();
+</script>
