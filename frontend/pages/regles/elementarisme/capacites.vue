1d40eda7d53d9431ba19b86bae0ac49175607b4a
Francis Pion <francispion@hotmail.com>
2026-01-13 23:03:14 -0500

    elementarisme

---
+++ frontend/pages/regles/elementarisme/capacites.vue
@@ -0,0 +1,22 @@
+<template>
+  <main class="container">
+    <h1>{{ title }}</h1>
+    <AppBreadcrumb :active="title" :parent="parent" />
+    <p>🚧</p>
+  </main>
+</template>
+
+<script setup lang="ts">
+import type { Breadcrumb } from "~/types/components";
+
+const parent: Breadcrumb[] = [
+  { text: "Annexes", to: "/regles/annexes" },
+  { text: "Élémentarisme", to: "/regles/elementarisme" },
+];
+const title: string = "Capacités élémentaires";
+
+useSeo({
+  title,
+  description: "🚧",
+});
+</script>
