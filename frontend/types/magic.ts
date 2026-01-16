60a0b3437a292318626f2ebffa82d2a414e4fdc4
Francis Pion <francispion@hotmail.com>
2026-01-16 16:37:16 -0500

    types

--- frontend/types/magic.ts
+++ frontend/types/magic.ts
@@ -1,3 +1,46 @@
+import type { Aggregate } from "./game";
+
+export type DurationUnit = "Day" | "Hour" | "Minute" | "Month " | "Round" | "Second" | "Week " | "Year";
+
+export type SpellNew = Aggregate & {
+  slug: string;
+  name: string;
+  tier: number;
+  summary?: string | null;
+  metaDescription?: string | null;
+  description?: string | null;
+  levels: SpellLevel[];
+}; // TODO(fpion): rename this class
+
+export type SpellCasting = {
+  time: string;
+  isRitual: boolean;
+};
+
+export type SpellComponents = {
+  isSomatic: boolean;
+  isVerbal: boolean;
+  focus?: string | null;
+  material?: string | null;
+};
+
+export type SpellDuration = {
+  value: number;
+  unit: DurationUnit;
+  isConcentration: boolean;
+};
+
+export type SpellLevel = {
+  level: number;
+  name?: string | null;
+  casting: SpellCasting;
+  duration?: SpellDuration | null;
+  range: number;
+  components: SpellComponents;
+  description?: string | null;
+};
+
+// TODO(fpion): LEGACY; remove the following
 export type Spell = {
   id: string;
   slug: string;
