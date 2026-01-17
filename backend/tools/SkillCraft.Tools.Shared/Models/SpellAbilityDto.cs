77bf6d9c0f5e41d15f72771947d7b7d331fd3c38
Francis Pion <francispion@hotmail.com>
2026-01-16 19:03:15 -0500

    dtos

---
+++ backend/tools/SkillCraft.Tools.Shared/Models/SpellAbilityDto.cs
@@ -0,0 +1,14 @@
+﻿namespace SkillCraft.Tools.Shared.Models;
+
+public record SpellAbilityDto
+{
+  public int Level { get; set; }
+  public string? Name { get; set; }
+
+  public SpellCastingDto Casting { get; set; } = new();
+  public SpellDurationDto? Duration { get; set; }
+  public int Range { get; set; }
+  public SpellComponentsDto Components { get; set; } = new();
+
+  public string? Description { get; set; }
+}
---
+++ backend/tools/SkillCraft.Tools.Shared/Models/SpellComponentsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SkillCraft.Tools.Shared.Models;
+
+public record SpellComponentsDto
+{
+  public string? Focus { get; set; }
+  public string? Material { get; set; }
+  public bool Somatic { get; set; }
+  public bool Verbal { get; set; }
+}
