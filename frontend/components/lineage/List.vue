3dc26c8207bf2062618619e041c243e7c3eec2dc
Francis Pion <francispion@hotmail.com>
2026-01-22 13:12:22 -0500

    Categorized Lineages.

--- frontend/components/lineage/List.vue
+++ frontend/components/lineage/List.vue
@@ -1,42 +1,38 @@
 <template>
   <ClientOnly>
-    <ListMode class="mb-4" v-model="mode" />
-    <div v-if="mode === 'grid'" class="row">
-      <div v-for="lineage in lineages" :key="lineage.id" class="col-xs-12 col-sm-6 col-md-4 col-lg-3 mb-4">
+    <div class="row">
+      <div v-for="lineage in lineages" :key="lineage.id" :class="classes">
         <LineageCard class="d-flex flex-column h-100" :lineage="lineage" />
       </div>
     </div>
-    <table v-else-if="mode === 'list'" class="table table-striped text-center">
-      <thead>
-        <tr>
-          <th scope="col" class="w-20">Espèce</th>
-          <th scope="col" class="w-80">Résumé</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="lineage in lineages" :key="lineage.id">
-          <td>
-            <NuxtLink :to="`/regles/especes/${lineage.slug}`">{{ lineage.name }}</NuxtLink>
-          </td>
-          <td>
-            <template v-if="lineage.summary">{{ lineage.summary }}</template>
-            <span v-else class="text-muted">{{ "—" }}</span>
-          </td>
-        </tr>
-      </tbody>
-    </table>
   </ClientOnly>
 </template>
 
 <script setup lang="ts">
+import { parsingUtils } from "logitar-js";
+
 import type { Lineage } from "~/types/lineages";
-import type { ListMode } from "~/types/components";
+
+const { parseNumber } = parsingUtils;
 
 const props = defineProps<{
+  cols?: number | string | null;
   items: Lineage[];
 }>();
 
-const mode = ref<ListMode>("grid");
-
+const classes = computed<string[]>(() => {
+  const classes: string[] = ["mb-4", "col-xs-12"];
+  const cols: number | undefined = parseNumber(props.cols ?? undefined) ?? 0;
+  if (cols >= 2) {
+    classes.push("col-sm-6");
+  }
+  if (cols >= 3) {
+    classes.push("col-md-4");
+  }
+  if (cols >= 4) {
+    classes.push("col-lg-3");
+  }
+  return classes;
+});
 const lineages = computed<Lineage[]>(() => props.items);
 </script>
--- frontend/pages/regles/especes/index.vue
+++ frontend/pages/regles/especes/index.vue
@@ -27,7 +27,80 @@
         <NuxtLink to="/regles/especes/taille">taille</NuxtLink>, poids et vieillissement.
       </li>
     </ul>
-    <LineageList :items="lineages" />
+    <h2 class="h3">Table des matières</h2>
+    <ul>
+      <li>
+        <a href="#common">Espèces communes</a>
+      </li>
+      <li>
+        <a href="#crossed">Espèces croisées</a>
+      </li>
+      <li>
+        <a href="#animals">Hommes-animaux</a>
+      </li>
+      <li>
+        <a href="#fairies">Fées</a>
+      </li>
+      <li>
+        <a href="#elementals">Élémentaires</a>
+      </li>
+      <li>
+        <a href="#horde">Horde</a>
+      </li>
+    </ul>
+    <h2 id="common" class="h3">Espèces communes</h2>
+    <p>
+      Ces espèces sont répandues aux quatre coins du continent. Cette présence universelle fait d’elles sont la norme démographique, culturelle et politique du
+      continent. Leurs sociétés ont façonné les peuplements, les routes, les institutions et l’économie locale, en plus de servir de référence linguistique et
+      sociale dans la majorité des régions. Malgré une forte cohésion sociale, ces sociétés sont néanmoins pensées d’abord et avant tout pour ces espèces. Les
+      autres espèces peuvent donc ressentir un profond décalage culture et structurel, allant jusqu’à l’exclusion.
+    </p>
+    <LineageList cols="4" :items="commonLineages" />
+    <h2 id="crossed" class="h3">Espèces croisées</h2>
+    <p>
+      Les espèces dites croisées sont issues de l’union de deux lignées distinctes. Leur héritage multiple leur confère des traits physiques, culturels ou
+      surnaturels variables, qui ne se manifestent jamais de manière uniforme. Leur degré d’intégration aux sociétés des espèces communes varie fortement en
+      fonction de l’individu. Certains s’insèrent aisément dans les sociétés de l’un ou l’autre de leurs parents, tandis que d’autres peinent à trouver leur
+      place, pris entre des normes sociales, biologiques ou symboliques parfois incompatibles. Leur identité se construit donc davantage par le vécu personnel
+      que par l’appartenance collective.
+    </p>
+    <LineageList cols="3" :items="crossedLineages" />
+    <h2 id="animals" class="h3">Hommes-animaux</h2>
+    <p>
+      Les hommes-animaux sont les autochtones de la région. Autrefois maîtres d’un vaste territoire, ils ont vu leurs terres se réduire au fil des siècles, sous
+      la pression de l’expansion des autres peuples. Face à ce recul, deux voies se sont imposées. Certains ont choisi le repli, vivant en marge des sociétés
+      établies afin de préserver leurs valeurs et traditions millénaires. D’autres se sont intégrés, volontairement ou sous contrainte, au prix d’une érosion
+      parfois profonde de leur culture ancestrale. Cette fracture marque encore aujourd’hui l’identité et le destin de ces peuples.
+    </p>
+    <LineageList cols="3" :items="animalLineages" />
+    <h2 id="fairies" class="h3">Fées</h2>
+    <p>
+      Les fées sont issues d’autres dimensions, lumineuses ou ombrageuses, et entretiennent un lien étroit avec les forces qui imprègnent le monde. Leur
+      existence ne s’organise ni autour des peuples mortels ni autour de leurs structures politiques, culturelles ou religieuses. Leurs territoires s’articulent
+      plutôt autour de lieux de pouvoir, véritables sources naturelles de magie. Elles y veillent avant tout à la préservation de ces sites et à l’équilibre des
+      forces qui s’y manifestent, agissant selon des logiques qui leur sont propres et souvent étrangères aux enjeux des sociétés du continent.
+    </p>
+    <LineageList :items="fairyLineages" />
+    <h2 id="elementals" class="h3">Élémentaires</h2>
+    <p>
+      Les élémentaires tirent leur origine de la présence d’esprits élémentaires venus d’autres dimensions. Leur existence est intimement liée à des lieux
+      fortement marqués par leur élément, auxquels ils s’attachent bien davantage qu’à des frontières, des organisations politiques, des religions ou des
+      cultures. De ce fait, ils demeurent généralement neutres dans les conflits militaires et politiques. Leurs actions sont guidées avant tout par l’intérêt
+      de leur espèce et la préservation de leur habitat, quitte à ignorer ou contrarier les enjeux des peuples qui les entourent.
+    </p>
+    <LineageList cols="4" :items="elementalLineages" />
+    <h2 id="horde" class="h3">Horde</h2>
+    <p>
+      La Horde désigne une confédération tribale aux contours flous et changeants, composée d’espèces habituellement considérées comme monstrueuses. Son
+      organisation varie fortement d’un clan à l’autre, sans autorité centrale durable ni structure politique unifiée. Arrivée récemment dans la région depuis
+      l’Est lointain du continent, la Horde se manifeste par des conquêtes et des raids dont la violence et la brutalité diffèrent selon l’espèce et le clan.
+      Les motivations qui les animent sont tout aussi hétérogènes : accumulation de richesses, contrôle de territoires ou de peuples, quête de prestige, défis
+      personnels ou autres ambitions propres à chaque individu.
+    </p>
+    <LineageList :items="hordeLineages" />
+    <button class="btn btn-lg btn-primary position-fixed bottom-0 end-0 m-3 rounded-circle" @click="scrollToTop">
+      <font-awesome-icon icon="fas fa-arrow-up" />
+    </button>
   </main>
 </template>
 
@@ -50,6 +123,24 @@ const { data } = await useLazyAsyncData<SearchResults<Lineage>>(
 );
 
 const lineages = computed<Lineage[]>(() => data.value?.items ?? []);
+
+const animalSlugs: Set<string> = new Set(["amphydre", "centaure", "harseme", "minotaure", "sarne", "skame"]);
+const animalLineages = computed<Lineage[]>(() => lineages.value.filter(({ slug }) => animalSlugs.has(slug)));
+
+const commonSlugs: Set<string> = new Set(["elfe", "humain", "nain", "petit-gens"]);
+const commonLineages = computed<Lineage[]>(() => lineages.value.filter(({ slug }) => commonSlugs.has(slug)));
+
+const crossedSlugs: Set<string> = new Set(["celestin", "demi-elfe", "demi-orque", "dhampir", "infernon", "sang-dragon"]);
+const crossedLineages = computed<Lineage[]>(() => lineages.value.filter(({ slug }) => crossedSlugs.has(slug)));
+
+const elementalSlugs: Set<string> = new Set(["gnome", "salamandre", "sylphe", "triton"]);
+const elementalLineages = computed<Lineage[]>(() => lineages.value.filter(({ slug }) => elementalSlugs.has(slug)));
+
+const fairySlugs: Set<string> = new Set(["satyre"]);
+const fairyLineages = computed<Lineage[]>(() => lineages.value.filter(({ slug }) => fairySlugs.has(slug)));
+
+const hordeSlugs: Set<string> = new Set(["gobelin"]);
+const hordeLineages = computed<Lineage[]>(() => lineages.value.filter(({ slug }) => hordeSlugs.has(slug)));
 
 useSeo({
   title,
