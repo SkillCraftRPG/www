99857951ab61167c850791a1b9c7292e2ce3bbee
Francis Pion <francispion@hotmail.com>
2026-02-18 19:28:04 -0500

    February 18th Work. (#160)

---
+++ components/elementalism/Expelling.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h3 id="expulsion-planaire" class="h5">Expulsion planaire</h3>
+    <p>
+      Une fois par <NuxtLink to="/regles/combat/deroulement/tour">tour</NuxtLink>, par une <NuxtLink to="/regles/combat/deroulement/tour">action</NuxtLink>, le
+      personnage peut dépenser un point d’<NuxtLink to="/regles/elementarisme/essence">essence élémentaire</NuxtLink> afin de tenter d’expulser une créature
+      qu’il peut voir et située à 9 mètres ou moins de sa position à la frontière entre le monde matériel et une dimension élémentaire, dans la dimension
+      spirituelle. La cible doit se trouver dans le monde matériel ou dans une dimension élémentaire.
+    </p>
+    <p>
+      Si la cible n’est pas consentante, le personnage effectue un <NuxtLink to="/regles/competences/tests">test</NuxtLink> d’<NuxtLink
+        to="/regles/competences/occultisme"
+        >Occultisme</NuxtLink
+      >, et la cible effectue un <NuxtLink to="/regles/competences/tests/sauvegarde">jet de sauvegarde</NuxtLink> de
+      <NuxtLink to="/regles/competences/discipline">Discipline</NuxtLink> de <NuxtLink to="/regles/competences/tests/difficulte">difficulté</NuxtLink> égale au
+      résultat du test. Le jet de sauvegarde est affligé du <NuxtLink to="/regles/competences/tests/avantage-desavantage">désavantage</NuxtLink> si la cible
+      provient de la dimension élémentaire. En cas d’échec, la cible est transportée dans la dimension spirituelle jusqu’au début du prochain tour du
+      personnage. L’emplacement est inoffensif envers celle-ci, et elle est <NuxtLink to="/regles/combat/conditions/incapable">incapable</NuxtLink> pendant
+      qu’elle s’y trouve. La cible réapparaît dans l’espace où elle se trouvait, ou dans l’<NuxtLink to="/regles/aventure/mouvement/espace-occupe"
+        >espace inoccupé</NuxtLink
+      >
+      le plus proche.
+    </p>
+  </div>
+</template>
---
+++ components/elementalism/MinorForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h3 id="incarnation-mineure" class="h5">Incarnation élémentaire mineure</h3>
+    <p>
+      Une fois par <NuxtLink to="/regles/combat/deroulement/tour">tour</NuxtLink>, par une <NuxtLink to="/regles/combat/deroulement/tour">action</NuxtLink>, le
+      personnage peut dépenser 3 points d’<NuxtLink to="/regles/elementarisme/essence">essence élémentaire</NuxtLink> afin d’assumer la forme d’un élémentaire
+      mineur (niveau&nbsp;2 et moins). Il peut reprendre sa forme normale par une action. Ses caractéristiques sont remplacées par celle de l’élémentaire.
+    </p>
+    <ul>
+      <li>
+        Les <NuxtLink to="/regles/combat/activites">activités</NuxtLink> qu’il peut accomplir et les <NuxtLink to="/regles/langues">langues</NuxtLink> qu’il
+        peut parler sont limitées à celles de l’élémentaire.
+      </li>
+      <li>
+        Il décide si son <NuxtLink to="/regles/equipement">équipement</NuxtLink> fusionne avec sa forme élémentaire ou s’il tombe au sol à ses pieds. Dans tous
+        les cas, il ne peut utiliser son équipement lorsqu’il assume cette forme.
+      </li>
+      <li>
+        S’il se <NuxtLink to="/regles/magie/parametres/duree">concentrait</NuxtLink> sur un <NuxtLink to="/regles/magie/pouvoirs">pouvoir</NuxtLink> au moment
+        où il se transforme, il perd la concentration et le pouvoir prend fin.
+      </li>
+      <li>Il ne peut utiliser de capacité élémentaire lorsqu’il assume cette forme.</li>
+      <li>
+        S’il <NuxtLink to="/regles/combat/mort-agonie">meurt</NuxtLink>, ou si les points de <NuxtLink to="/regles/statistiques/vitalite">Vitalité</NuxtLink> de
+        l’élémentaire chutent à 0, il reprend sa forme normale, et les <NuxtLink to="/regles/combat/degats">points de dégâts</NuxtLink> excédentaires sont
+        transféré à sa forme normale.
+      </li>
+      <li>
+        Il peut rester dans cette forme pendant un nombre d’<NuxtLink to="/regles/aventure/temps">heures</NuxtLink> égal à ses
+        <NuxtLink to="/regles/attributs/sens">Sens</NuxtLink> (minimum&nbsp;1). Après ce délai, il reprend sa forme normale.
+      </li>
+    </ul>
+  </div>
+</template>
